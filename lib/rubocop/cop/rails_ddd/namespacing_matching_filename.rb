3ac21f2713a4d7c572e74db104072820de27090e
Mark Burns <markthedeveloper@gmail.com>
2017-11-07 16:10:49 +0000

    initial passing specs

--- lib/rubocop/cop/rails_ddd/namespacing_matching_filename.rb
+++ lib/rubocop/cop/rails_ddd/namespacing_matching_filename.rb
@@ -23,6 +23,114 @@ module RuboCop
       #   app/concepts/some_folder/file.rb    # module SomeFolder; class File; end; end
       #
       class NamespacingMatchingFilename < Cop
+        def on_module(node)
+          return unless concepts_folder?
+
+          return if correct_module_name?(SingleConstantFinder.for node: node)
+
+          add_offense(node, location: :expression, message: message)
+        end
+
+        def on_class(node)
+          return unless concepts_folder?
+
+          return if valid_fully_qualified_constant_exists_in_file?(node)
+
+          add_offense(node, location: :expression, message: message)
+        end
+
+        def valid_fully_qualified_constant_exists_in_file?(node)
+          constants = ConstantFinder.for(node: node.ancestors.last)
+          constants.any?{|c| ValidConstantPathDetermination.valid_fully_qualified_constant?(c, path) }
+        end
+
+        private
+
+        def correct_module_name?(constant_name)
+          ValidConstantPathDetermination.valid?(constant_name, path)
+        end
+
+        def message
+          "Incorrect constant name for #{path}".freeze
+        end
+
+        def concepts_folder?
+          path.match %r{app/concepts/}
+        end
+
+        def path
+          processed_source.path
+        end
+
+        module SingleConstantFinder
+          def self.for(node: node, namespace: nil)
+            return nil unless node.is_a? RuboCop::AST::Node
+            return nil if node.type == :const
+
+            infer_namespace(node)
+          end
+
+          def self.infer_namespace(node, const_chain: [])
+            return "::" + const_chain.compact.reverse.join("::") if node.nil?
+
+            const_name = if node.class_type? || node.module_type?
+              node.children.first.const_name
+            end
+
+            const_chain << const_name
+
+            infer_namespace(node.parent, const_chain: const_chain)
+          end
+
+        end
+
+        module ConstantFinder
+          def self.for(node: node, constants: [], namespace: "")
+            return constants unless node.is_a? RuboCop::AST::Node
+            return constants if node.type == :const
+
+            if node.module_type? || node.class_type?
+              qualified_const_name = for_node(node: node, namespace: namespace)
+              constants << qualified_const_name
+            else
+              qualified_const_name = namespace
+            end
+
+            node.children.each do |n|
+              constants = self.for(node: n, constants: constants, namespace: qualified_const_name)
+            end
+
+            constants.compact
+          end
+
+          def self.for_node(node: node, namespace: nil)
+            return nil unless node.is_a? RuboCop::AST::Node
+            return nil if node.type == :const
+
+            if node.module_type? || node.class_type?
+              [namespace, node.children.first.const_name].compact.join("::")
+            end
+          end
+        end
+
+        module ValidConstantPathDetermination
+          def self.valid_fully_qualified_constant?(const_name, path)
+            expected_const_name(path) == const_name
+          end
+
+          def self.valid?(const_name, path)
+            expected_const_name(path).starts_with? const_name
+          end
+
+          def self.expected_const_name(path)
+            "::" +
+              path.
+              gsub(%r{app/concepts/}, "").
+              gsub(%r{\.rb\z}, "").
+              camelize
+          end
+        end
+
         module ConstantNameFinder
           def self.for(class_name)
             # There is probably a prettier way to do this with some
@@ -32,67 +140,13 @@ module RuboCop
             constants = []
 
             length.times.each do |i|
-              constants.push levels[0..i].join("::")
+              constant_name = levels[0..i].join("::")
+              constant_name = "::#{constant_name}" unless constant_name.starts_with? '::'
+              constants.push constant_name
             end
 
             constants
           end
-        end
-
-        def on_module(node)
-          return unless concepts_folder?
-          return if !!node.parent
-
-          return if correct_module_name?(node)
-
-          add_offense(node, location: :expression, message: message)
-        end
-
-        private
-
-        def correct_module_name?(node)
-          valid_constant_name?(node) && feasible_path?(node)
-        end
-
-        def valid_constant_name?(node)
-          valid_constant_names.include? module_name(node)
-        end
-
-        def feasible_path?(node)
-          expected_filename(node) == path || 
-            path.match(%r{app/concepts/#{module_name(node).underscore}/.*\.rb})
-        end
-
-        def expected_filename(node)
-          "app/concepts/" + module_name(node).underscore + ".rb"
-        end
-
-        def module_name(node)
-          (node.ancestors + [node]).map(&:defined_module_name).join("::")
-        end
-
-        def message
-          "Incorrect constant name for #{path}".freeze
-        end
-
-        def valid_constant_names
-          ConstantNameFinder.for(full_expected_constant_name)
-        end
-
-        def full_expected_constant_name
-          path_name.gsub(/\.rb\z/, "").camelize
-        end
-
-        def path_name
-          path.gsub(%r{\Aapp/concepts/}, "")
-        end
-
-        def concepts_folder?
-          path.match %r{app/concepts/}
-        end
-
-        def path
-          processed_source.path
         end
       end
     end
--- lib/rubocop/cop/rails_ddd/namespacing_matching_filename_spec.rb
+++ lib/rubocop/cop/rails_ddd/namespacing_matching_filename_spec.rb
@@ -23,7 +23,7 @@ RSpec.describe RuboCop::Cop::RailsDdd::NamespacingMatchingFilename, :config do
   context "with a correct filename" do
     let(:expected) do
       <<-OUTPUT
-          module SomeFile; end
+        module SomeFile; end
       OUTPUT
     end
 
@@ -61,18 +61,221 @@ RSpec.describe RuboCop::Cop::RailsDdd::NamespacingMatchingFilename, :config do
     end
   end
 
-  describe described_class::ConstantNameFinder do
-    it do
-      expect(described_class.for("SomeFile")).to eq ["SomeFile"]
-      expected_results = [
-        "SomeFile",
-        "SomeFile::SomeNamespace",
-        "SomeFile::SomeNamespace::AnotherNamespace",
-        "SomeFile::SomeNamespace::AnotherNamespace::FinalConstant"
-      ]
+  context "with an invalid class name in the file" do
+    let(:expected) do
+      <<-OUTPUT
+        module SomeFile
+          module AnotherConstant
+            class IncorrectName
+            ^^^^^^^^^^^^^^^^^^^ Incorrect constant name for app/concepts/some_file/another_constant/yet_another_constant.rb
+            end
 
-      expect(described_class.for("SomeFile::SomeNamespace::AnotherNamespace::FinalConstant")).to eq expected_results
+            class AnotherWrongName
+            ^^^^^^^^^^^^^^^^^^^^^^ Incorrect constant name for app/concepts/some_file/another_constant/yet_another_constant.rb
+            end
+          end
+        end
+      OUTPUT
     end
 
+    it do
+      expect_offense(expected, path)
+    end
+
+    let(:source) { expected.gsub(/^\s*\^.*$/, "") }
+
+      require 'parser/current'
+
+    let(:node) do
+      # example from http://www.rubydoc.info/gems/rubocop/RuboCop/AST/Builder
+      buffer = Parser::Source::Buffer.new('')
+      buffer.source = source
+
+      builder = RuboCop::AST::Builder.new
+      parser = Parser::CurrentRuby.new(builder)
+      parser.parse(buffer)
+    end
+
+    let(:path) { 'app/concepts/some_file/another_constant/yet_another_constant.rb' }
+
+    describe "#valid_constant_exists_in_file?" do
+      before do
+        allow(cop).to receive(:path).and_return path
+      end
+
+      it do
+        expect(cop.valid_fully_qualified_constant_exists_in_file?(node)).to eq false
+      end
+    end
+
+    describe described_class::ValidConstantPathDetermination do
+      let(:valid) { described_class.valid?(const_name, path) }
+
+      context "with a top level constant" do
+        let(:const_name) { "::TopLevel" }
+        let(:path) { "app/concepts/top_level.rb" }
+        it do
+          expect(valid).to be true
+        end
+      end
+
+      context "with a nested constant" do
+        let(:const_name) { "::TopLevel::Nested" }
+        let(:path) { "app/concepts/top_level/nested.rb" }
+        it do
+          expect(valid).to be true
+        end
+      end
+
+      context "a top level constant in a nested path" do
+        let(:const_name) { "::TopLevel" }
+        let(:path) { "app/concepts/top_level/nested.rb" }
+        it do
+          expect(valid).to be true
+        end
+      end
+
+      context "with nonsense constant name" do
+        let(:const_name) { "123" }
+        let(:path) { "app.rb" }
+        it do
+          expect(valid).to be false
+        end
+      end
+
+      context "with nonsense constant name matching the path" do
+        let(:const_name) { "123" }
+        let(:path) { "123.rb" }
+        it do
+          expect(valid).to be false
+        end
+      end
+
+      context "an incorrect top level constant in a nested path" do
+        let(:const_name) { "::Wrong::Nested" }
+        let(:path) { "app/concepts/top_level/nested.rb" }
+        it do
+          expect(valid).to be false
+        end
+      end
+
+      context "an incorrect nested constant" do
+        let(:const_name) { "::TopLevel::Wrong" }
+        let(:path) { "app/concepts/top_level/nested.rb" }
+        it do
+          expect(valid).to be false
+        end
+      end
+    end
+
+    describe described_class::SingleConstantFinder do
+
+      context "with child class node" do
+        let(:nested_node) { node.children.last.children.last }
+        let(:constant) { described_class.for(node: nested_node) }
+        let(:source) do
+          <<-RUBY
+            module Outer
+              module Inner
+                class SomeClass
+                end
+              end
+            end
+          RUBY
+        end
+
+        it do
+          expect(constant).to eq "::Outer::Inner::SomeClass"
+        end
+      end
+    end
+
+    describe described_class::ConstantFinder do
+      let(:constants) { described_class.for(node: node) }
+
+      it do
+        expect(constants).to match_array [
+          "::SomeFile",
+          "::SomeFile::AnotherConstant",
+          "::SomeFile::AnotherConstant::IncorrectName",
+          "::SomeFile::AnotherConstant::AnotherWrongName",
+        ]
+      end
+
+      context "with complex example" do
+        let(:source) do
+          <<-RUBY
+          class Egg
+          end
+            class Dog
+            end
+            class Another
+              class Nested
+              end
+            end
+            class Something
+              include Thing
+              def self.asdf(a, b=2, asdf: qwer)
+              end
+              class << self
+                module InsideMetaClass
+                end
+              end
+              module This
+                attr_reader :anything
+                class That
+                end
+              end
+            end
+              RUBY
+        end
+        it do
+          expect(constants).to match_array [
+            "::Egg",
+            "::Dog",
+            "::Another",
+            "::Another::Nested",
+            "::Something",
+            "::Something::InsideMetaClass",
+            "::Something::This",
+            "::Something::This::That"
+          ]
+        end
+        end
+      end
+    end
+
+    context "with other constants in the file" do
+      let(:expected) do
+        <<-OUTPUT
+        module SomeFile
+          module AnotherConstant
+            class YetAnotherConstant
+            end
+
+            class AcceptableYetUnpredictableClassName
+            end
+          end
+        end
+        OUTPUT
+      end
+
+      it do
+        expect_no_offenses(expected, 'app/concepts/some_file/another_constant/yet_another_constant.rb')
+      end
+    end
+
+    describe described_class::ConstantNameFinder do
+      it do
+        expect(described_class.for("SomeFile")).to eq ["::SomeFile"]
+        expected_results = [
+          "::SomeFile",
+          "::SomeFile::SomeNamespace",
+          "::SomeFile::SomeNamespace::AnotherNamespace",
+          "::SomeFile::SomeNamespace::AnotherNamespace::FinalConstant"
+        ]
+
+        expect(described_class.for("SomeFile::SomeNamespace::AnotherNamespace::FinalConstant")).to eq expected_results
+      end
+    end
   end
-end

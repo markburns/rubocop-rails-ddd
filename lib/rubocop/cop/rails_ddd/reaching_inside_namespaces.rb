62b7475429da79540fe5adda1772d1f44d414e48
Mark Burns <markthedeveloper@gmail.com>
2018-11-17 07:26:09 +0000

    add extra test for reaching inside namespaces

--- lib/rubocop/cop/rails_ddd/reaching_inside_namespaces.rb
+++ lib/rubocop/cop/rails_ddd/reaching_inside_namespaces.rb
@@ -2,14 +2,14 @@ module RuboCop
   module Cop
     module RailsDdd
       class ReachingInsideNamespaces < Cop
-        def on_send(node)
-          return unless nested_constants? node.children.first
+        def on_const(node)
+          return unless nested_constants? node
           message = "Don't reach inside other namespaces. Refactor to avoid this or provide a public aggregate root method."
-          lib/rubocop/cop/rails_ddd/reaching_inside_namespaces.rbadd_offense(node, location: :expression, message: message)
+          add_offense(node, location: :expression, message: message)
         end
 
         def nested_constants?(node)
-          node.type == :const && node.children.first.type == :const
+          node.type == :const && node&.children&.first&.type == :const
         end
       end
     end
--- lib/rubocop/cop/rails_ddd/reaching_inside_namespaces_spec.rb
+++ lib/rubocop/cop/rails_ddd/reaching_inside_namespaces_spec.rb
@@ -4,13 +4,28 @@ require 'parser/current'
 RSpec.describe RuboCop::Cop::RailsDdd::ReachingInsideNamespaces, :config do
   subject(:cop) { described_class.new(config) }
 
-  context 'with an incorrect filename' do
+  context 'referring to a nested constant in another namespace' do
+    let(:expected) do
+      <<-OUTPUT
+        module Namespace
+          klass = ::AnotherNamespace::AnotherClass
+                  ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ Don't reach inside other namespaces. Refactor to avoid this or provide a public aggregate root method.
+        end
+      OUTPUT
+    end
+
+    it do
+      expect_offense(expected, 'app/concepts/namespace.rb')
+    end
+  end
+
+  context 'calling a method on a nested constant' do
     let(:expected) do
       <<-OUTPUT
         module Namespace
           def self.a_method
             ::AnotherNamespace::AnotherClass.some_method
-            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ Don't reach inside other namespaces. Refactor to avoid this or provide a public aggregate root method.
+            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ Don't reach inside other namespaces. Refactor to avoid this or provide a public aggregate root method.
           end
         end
       OUTPUT
